8df0a1a2ade3b8cace4ffbdd9fff61f2c572a410
B2Hacker <brandon.cortes.1690@gmail.com>
2020-10-21 17:43:37 -0500

    13th: Checking NavBar and using Nacho's components

--- components/NavBar.js
+++ components/NavBar.js
@@ -31,81 +31,27 @@ export default function NavBar(props) {
 
     return (
         <>
-
             <div className={classes.rootAppBar}>
-                <Navbar fixed="top" expanded={expanded} collapseOnSelect expand="md" bg="dark" variant="dark" className="pl-2 pr-2">
-                    <Navbar.Brand href="/">Inurance Info</Navbar.Brand>
-                    <>
-                        <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => setExpanded(expanded ? false : true)} />
-                        <Navbar.Collapse id="responsive-navbar-nav">
-                            <Nav className="mr-auto" onClick={toggleMenuExpanded}>
-                                <Link href="/items"><a className="nav-link">Items</a></Link>
-                                <NavDropdown title="Setup" id="basic-nav-dropdown" show={expanded || menuExpanded}>
-
-                                    <Link href="/categories">
-                                        <NavDropdown.Item href="/categories">
-                                            Categories
-                      </NavDropdown.Item>
-                                    </Link>
-
-                                    <Link href="/conditions">
-                                        <NavDropdown.Item href="/conditions">
-                                            Conditions
-                      </NavDropdown.Item>
-                                    </Link>
-
-                                    <NavDropdown.Divider />
-
-                                    <Link href="/locations">
-                                        <NavDropdown.Item href="/locations">
-                                            Locations
-                      </NavDropdown.Item>
-                                    </Link>
-
-                                    <Link href="/rooms">
-                                        <NavDropdown.Item href="/rooms">
-                                            Rooms
-                      </NavDropdown.Item>
-                                    </Link>
-
-                                    <NavDropdown.Divider />
-
-                                    <Link href="/contacts">
-                                        <NavDropdown.Item href="/contacts">
-                                            Contacts
-                      </NavDropdown.Item>
-                                    </Link>
-
-                                    <Link href="/companies">
-                                        <NavDropdown.Item href="/companies">
-                                            Companies
-                      </NavDropdown.Item>
-                                    </Link>
-
-                                    <NavDropdown.Divider />
-
-                                    <Link href="/contracts">
-                                        <NavDropdown.Item href="/contracts">
-                                            Contracts
-                      </NavDropdown.Item>
-                                    </Link>
-
-                                    <Link href="/policies">
-                                        <NavDropdown.Item href="/policies">
-                                            Policies
-                      </NavDropdown.Item>
-                                    </Link>
-
-                                </NavDropdown>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </>
-                </Navbar>
-
-                {/* FOOTER */}
-                <Navbar fixed="bottom" bg="dark" variant="dark" className="pl-2 pr-2 justify-content-between">
-                    <Navbar.Text>myInventory @ 2020</Navbar.Text>
-                    <Navbar.Text>version 1.1</Navbar.Text>
+                <Navbar collapseOnSelect expanded={expanded} expand="md" bg="dark" variant="dark">
+                    <Navbar.Brand href="/">Insurance Info</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => setExpanded(expanded ? false : true)}/>
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mr-auto" onClick={toggleMenuExpanded}>
+                            <Nav.Link href="/categories">Categories</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">More deets</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">Dank memes</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Navbar>
             </div>
         </>
