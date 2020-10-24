ec41160ab7ffa5d3cd7c08dee3fcdfc99fe9e10c
B2Hacker <brandon.cortes.1690@gmail.com>
2020-10-24 16:12:01 -0500

    18th: Datatable with Categories data

--- src/pages/categories.js
+++ src/pages/categories.js
@@ -1,9 +1,49 @@
-import styles from "../../styles/Home.module.css"
+//import styles from "../../styles/Home.module.css"
+import { viewAllCategories } from "../../src/lib/apiCategory";
 
 export default function categoriesPage() {
+
+    const [allCategories, setAllCategories] = React.useState([]);
+
+
+    React.useEffect(() => {
+        getCategories();
+    }, []);
+
+    const getCategories = () => {
+        viewAllCategories().then(categories => {
+            setAllCategories(categories);
+        });
+    }
+
+
     return (
-        <div className={styles.container}>
-        <h1>Categories goes here!</h1>
+
+        <div>
+            <table className="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>SubCategory</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {allCategories.map(category => (
+                        <tr key={category._id}>
+                            <td>{category.name}</td>
+                            <td>{category.description}</td>
+                            <td>{Object.keys(category.subCategories).length}</td>
+                            <td>
+                                <button className="btn btn-primary">Edit</button>
+                                &nbsp;
+                                <button className="btn btn-danger">Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
 
     );
